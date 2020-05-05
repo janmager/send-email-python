e4716c7c389cbe6656ac09cf7c3806c0af0cb126
Jan Mager <kontakt@janmager.pl>
2020-05-05 13:42:49 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.message import EmailMessage
+
+yourAdress = 'your email'
+yourPassword = 'your password'
+
+receiverEmail = 'receiver email'
+subject = 'subject'
+content = 'content'
+
+smtp = 'your.smtp.com'
+port = 111
+
+msg = EmailMessage()
+msg['Subject'] = subject
+msg['From'] = yourAdress
+msg['To'] = receiverEmail
+
+msg.set_content(content)
+
+with smtplib.SMTP_SSL(smtp, port) as smtp:
+    smtp.login(yourAdress, yourPassword)
+    smtp.send_message(msg)
