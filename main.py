de87f1902244fb8415e64a82c223060b407c5bec
Jan Mager <kontakt@janmager.pl>
2020-05-05 13:44:32 +0200

    main.py update

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import smtplib
 from email.message import EmailMessage
 
+# Settings
 yourAdress = 'your email'
 yourPassword = 'your password'
-
 receiverEmail = 'receiver email'
 subject = 'subject'
 content = 'content'
-
 smtp = 'your.smtp.com'
 port = 111
 
+# Sending
 msg = EmailMessage()
 msg['Subject'] = subject
 msg['From'] = yourAdress
